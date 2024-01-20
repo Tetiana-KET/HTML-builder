049f2042c4cb9251894172ae7797634f354e1f06
tatiana <belangelphone@gmail.com>
2024-01-20 20:13:54 +0200

    add solution to 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourceFolderDir = path.join(__dirname, 'styles');
+const targetDir = path.join(__dirname, 'project-dist', 'bundle.css');
+const writeableStream = fs.createWriteStream(targetDir, 'utf8');
+fs.readdir(sourceFolderDir, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+
+  files.forEach((file) => {
+   console.log(file);
+    const filePath = path.join(sourceFolderDir, file.name);
+    const fileExt = path.parse(filePath).ext.slice(1);
+
+    if (file.isFile() && fileExt === 'css') {
+      fs.createReadStream(filePath, 'utf8').pipe(writeableStream);
+    }
+  });
+});
